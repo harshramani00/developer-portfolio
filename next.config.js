d32ad225fcfd28563c946bba67e8b3ba8eeed09b
harshramani00 <harshramani76@gmail.com>
2025-04-16 19:44:42 -0400

    Fix path issues for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
-const repo = 'developer-portfolio'; // your GitHub repo name
+const isGithubPages = process.env.DEPLOY_ENV === 'GH_PAGES';
+const repo = 'developer-portfolio'; // Your GitHub repo name
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  reactStrictMode: false,
   trailingSlash: true,
   images: {
     unoptimized: true,
+    domains: ['avatars.githubusercontent.com'],
   },
-  basePath: `/${repo}`,
-  assetPrefix: `/${repo}/`,
+  basePath: isGithubPages ? `/${repo}` : '',
+  assetPrefix: isGithubPages ? `/${repo}/` : '',
 };
 
 module.exports = nextConfig;
